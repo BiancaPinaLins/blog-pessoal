9bfbaa455dd45037cc93cdf1f6d9abcdaf55e21b
BiancaPinaLins <bianca_palins@outlook.com>
2022-02-16 14:50:33 -0300

    Blog pessoal - frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from './paginas/home/Home';
+import Home from './pages/home/Home';
 import Navbar from "./components/static/navbar/Navbar";
 import Footer from "./components/static/footer/Footer";
 import './App.css';
